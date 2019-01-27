fe334360e36a03537e66b3d3c3501092300d21fc
Ebrima Tunkara <etunkara51@gmail.com>
2019-01-26 23:59:38 -0500

    Add Cross Origin Annotation.

--- src/main/java/com/nbafantasy/rankings/controller/PlayerController.java
+++ src/main/java/com/nbafantasy/rankings/controller/PlayerController.java
@@ -4,12 +4,14 @@ import com.nbafantasy.rankings.model.Player;
 import com.nbafantasy.rankings.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(PlayerController.PATH)
 public class PlayerController {
